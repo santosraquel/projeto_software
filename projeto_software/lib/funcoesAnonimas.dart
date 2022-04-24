ed016538c12a190f2a9e661a2cae94b655f8a581
santosraquel <raquel.dossantos1965@gmail.com>
2022-04-24 00:11:45 -0300

    aula sobre funcoes anonimas

--- projeto_software/lib/funcoesAnonimas.dart
+++ projeto_software/lib/funcoesAnonimas.dart
@@ -6,51 +6,127 @@
   Informar:
     - Nome do aluno
     - Disciplina
+    - As 3 notas da disciplinas
+    - Os 3 pesos
     - Média para aprovação na diciplina
     Retornar "Relatório" contendo:
      - nome do aluno
      - nome da disciplinas
      - a média para aprovação na disciplina
-     - as notas informadas
-     - a média das notas
-     - se o aluno foi aprovado ou não
+     - a média artimética das notas e se o aluno foi ou não aprovado 
+     - a média ponderada e se o aluno foi ou não aprovado 
+
+  RELATÓRIO DA AULA:
+  Contexto 
+    A função anônima geralmente é utilizada quando temos uma função 
+    que possui dados e que possui diversas ações que variam. 
+
+  O que é?
+  Como o próprio nome já diz, função anônima
+  é uma função que não possui nome.
+
+  Quando utilizar?
+    Utilizamos uma função anônima quando temos um código que irá precisar ser
+    executado uma única vez dentro do sistema.
+
+  Síntaxe:
+    () {
+      ... instruções
+    }
+
+    Como chamar funções anônimas?
+    Por se tratar de uma função que não possui nome não conseguimos chamá-la 
+    diretamente, sendo assim é necessário atribui-la a uma variável e então chamar
+    por meio desta variável.
 
 */
 import 'dart:io';
 
-String show() {
-  String resultado = verificarAprovacaoDisciplina(1, 5.5, 6.0, 8.9, 7.0);
-  print(resultado);
+void show() {
+  dadosAluno();
 }
 
 String verificarAprovacaoDisciplina(int opcao, double nota1, double nota2,
-    double nota3, double mediaDisciplina) {
+    double nota3, int peso1, int peso2, int peso3, double mediaDisciplina) {
   if (opcao == 1) {
-    return calcularMedia(nota1, nota2, nota3, mediaDisciplina);
+    return calcularMediaAritmetica(nota1, nota2, nota3, mediaDisciplina);
+  }
+  if (opcao == 2) {
+    return calcularMediaPonderada(
+        nota1, nota2, nota3, peso1, peso2, peso3, mediaDisciplina);
   } else {
-    return dadosAluno();
+    return 'Opção inválida';
   }
 }
 
-String calcularMedia(
+String calcularMediaAritmetica(
     double nota1, double nota2, double nota3, double mediaDisciplia) {
   double media = (nota1 + nota2 + nota3) / 3;
   if (media >= mediaDisciplia) {
-    return 'APROVADO';
+    return '$media (APROVADO)';
   } else {
-    return 'REPROVADO';
+    return '$media (REPROVADO)';
+  }
+}
+
+String calcularMediaPonderada(double nota1, double nota2, double nota3,
+    int peso1, int peso2, int peso3, double mediaDisciplia) {
+  double media = ((nota1 * peso1) + (nota2 * peso2) + (nota3 * peso3)) /
+      (peso1 + peso2 + peso3);
+  if (media >= mediaDisciplia) {
+    return '$media (APROVADO)';
+  } else {
+    return '$media (REPROVADO)';
   }
 }
 
 void dadosAluno() {
+  print('Nome do Aluno:');
+  String nomeAluno = stdin.readLineSync()!;
+
+  print('Nome da disciplina: ');
+  String nomeDisciplina = stdin.readLineSync()!;
+
+  print('Nota 1: ');
+  String nt1 = stdin.readLineSync()!;
+  double nota1 = double.parse(nt1);
+
+  print('Nota 2: ');
+  String nt2 = stdin.readLineSync()!;
+  double nota2 = double.parse(nt2);
+
+  print('Nota 3: ');
+  String nt3 = stdin.readLineSync()!;
+  double nota3 = double.parse(nt3);
+
+  print('Peso 1: ');
+  String ps1 = stdin.readLineSync()!;
+  int peso1 = int.parse(ps1);
+
+  print('Peso 2: ');
+  String ps2 = stdin.readLineSync()!;
+  int peso2 = int.parse(ps2);
+
+  print('Nota 3: ');
+  String ps3 = stdin.readLineSync()!;
+  int peso3 = int.parse(ps3);
+
+  print('Média da disciplina: ');
+  String media = stdin.readLineSync()!;
+  double mediaDisciplina = double.parse(media);
+
+  String mediaArtimetica = verificarAprovacaoDisciplina(
+      1, nota1, nota2, nota3, 0, 0, 0, mediaDisciplina);
+
+  String mediaPonderada = verificarAprovacaoDisciplina(
+      2, nota1, nota2, nota3, peso1, peso2, peso3, mediaDisciplina);
   print('''
-          ================= DADOS ==================
-          Aluno: $nomeAluno,
-          Disciplina: $nomeDisciplina,
-          Média para aprovação: $mediaDisciplina,
-          Notas: $notasAluno,
-          Média notas: $mediaNotas,
-          Situação: $situacao, 
-          ==========================================
+        ======================= DADOS DO ALUNO ============================
+              Aluno(a): $nomeAluno
+              Disciplina: $nomeDisciplina
+              Média da Disciplina: $mediaDisciplina
+              Média Aritmética: $mediaArtimetica
+              Média Ponderada: $mediaPonderada
+        ===================================================================
 ''');
 }
